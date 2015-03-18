3414d247a565557edb97ac018f961938086b6853
sainath-aptuz <ak.sainath@aptuz.com>
2015-03-18 17:14:55 +0530

    Update base.py

--- funguilds/funguilds/settings/base.py
+++ funguilds/funguilds/settings/base.py
@@ -163,7 +163,7 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST_USER = 'partha@aptuz.com'
-EMAIL_HOST_PASSWORD = 'openPARDHU@06'
+EMAIL_HOST_PASSWORD = '****'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
